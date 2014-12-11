2f66c4d913bfe95981a6a20b365017523ca98ad6
Luke Pukatch <lpukatch@gmail.com>
2014-12-11 14:56:44 -0500

    inflated menu

--- src/edu/miami/masonandluke/phlogging/MainActivity.java
+++ src/edu/miami/masonandluke/phlogging/MainActivity.java
@@ -3,6 +3,7 @@ package edu.miami.masonandluke.phlogging;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
@@ -14,5 +15,13 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		phlogDB = new PhloggingDB(this);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
 	}
 }
